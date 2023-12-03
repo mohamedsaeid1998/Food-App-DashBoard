b6972cfd3fe7a8f217f126d95cf9bb560a475afa
mohamedsaeid1998 <105775732+mohamedsaeid1998@users.noreply.github.com>
2023-12-03 20:51:26 +0200

    message

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // resolve: {
+  resolve: {
     
-  //   alias: {
-  //     '@': '/src',
-  //   }
-  // },
+    alias: {
+      '@': '/src',
+    }
+  },
   plugins: [react()],
   base: /Food-App-DashBoard/
   
