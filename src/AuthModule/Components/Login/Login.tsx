37da910943520f337d53db8ba525108757b7284a
mohamedsaeid1998 <105775732+mohamedsaeid1998@users.noreply.github.com>
2024-03-14 11:27:51 +0200

    Improve Code

--- src/AuthModule/Components/Login/Login.tsx
+++ src/AuthModule/Components/Login/Login.tsx
@@ -19,9 +19,9 @@ const Login = ({ saveAdminData }: Props) => {
 
   const navigate = useNavigate()
 
-  const { register, handleSubmit, formState: { errors } } = useForm<IFormValues>()
+  const { register, handleSubmit,setValue, formState: { errors } } = useForm<IFormValues>()
 
-  function handleCallbackResponse(response:any){
+  function handleCallbackResponse(response: any) {
     localStorage.setItem("adminToken", response.credential)
     saveAdminData()
     navigate('/dashboard')
@@ -30,22 +30,22 @@ const Login = ({ saveAdminData }: Props) => {
       theme: "colored",
     });
 
-    }
+  }
 
   useEffect(() => {
-      // @ts-ignore
-  const google = window.google
+    // @ts-ignore
+    const google = window.google
     google.accounts.id.initialize({
-      client_id:'320174446901-75vhr20bt9ei5l896ul3lldh92bshbdg.apps.googleusercontent.com',
+      client_id: '320174446901-75vhr20bt9ei5l896ul3lldh92bshbdg.apps.googleusercontent.com',
       callback: handleCallbackResponse,
     })
 
-        google.accounts.id.renderButton(
+    google.accounts.id.renderButton(
       document.getElementById("signInDiv"),
-      {theme:"outline", size:"large"},
-  )
-        google.accounts.id.prompt()
-        
+      { theme: "outline", size: "large" },
+    )
+    google.accounts.id.prompt()
+
   }, [])
 
 
@@ -73,14 +73,24 @@ const Login = ({ saveAdminData }: Props) => {
         setLoading(false)
       })
 
-
   }
 
+
+
+
+
+
+
+
+
+
+
+
   return <>
-  <Helmet>
-  <title> Sign in • Food App </title>
-</Helmet>
-    <AuthComponent>
+    <Helmet>
+      <title> Sign in • Food App </title>
+    </Helmet>
+    <AuthComponent setValue={setValue}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <h2 className='fw-bold'>Log In</h2>
         <p>Welcome Back! Please enter your details</p>
@@ -91,13 +101,15 @@ const Login = ({ saveAdminData }: Props) => {
 
 
         <div className=' mt-2 d-flex flex-md-row justify-content-between align-items-center flex-column'>
-                  <div className=' my-2' id='signInDiv'></div>
+          <div className=' my-2' id='signInDiv'></div>
           <Link to={'/forget-pass-request'} className='forget'>Forgot Password ?</Link>
         </div>
         <button type='submit' disabled={Loading} className='btn btn-success w-100 mt-4 fw-bold'>{Loading ? <i className='fa fa-spin fa-spinner'></i> : "Login"}</button>
 
       </form>
+      
     </AuthComponent>
+    
   </>
 }
 
